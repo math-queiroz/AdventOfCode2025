f7e66f12ec7820dd8c2365603171d5ad765ac638
Matheus Queiroz <matheus.queiroz@autbank.com.br>
2025-12-02 15:34:47 -0300

    chore: ran rustfmt on day 01

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -5,16 +5,21 @@ fn main(input: String, line_ending: &str) -> (usize, usize) {
     let (mut dial, mut zero_count, mut flip_count): (isize, usize, usize) = (50, 0, 0);
     for line in parsed_data {
         let (dir, shift) = line.split_at(1);
-        let shift = shift.parse::<isize>()
+        let shift = shift
+            .parse::<isize>()
             .map(|n| if dir == "L" { -n } else { n })
             .expect("bad input");
         let target_dial = dial + shift;
 
         flip_count += target_dial.abs() as usize / 100;
-        if dial != 0 && target_dial <= 0 { flip_count += 1 }
+        if dial != 0 && target_dial <= 0 {
+            flip_count += 1
+        }
 
         dial = target_dial.rem_euclid(100);
-        if dial == 0 { zero_count += 1 }
+        if dial == 0 {
+            zero_count += 1
+        }
     }
     (zero_count, flip_count)
 }
