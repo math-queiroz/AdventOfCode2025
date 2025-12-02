9d1b33810b5e9b0af06657733467b1f340a6e998
Matheus Queiroz <matheus.queiroz@autbank.com.br>
2025-12-02 15:34:22 -0300

    chore: fixed template warnings

--- src/bin/template.rs
+++ src/bin/template.rs
@@ -1,8 +1,8 @@
 #[aoc::day(01, "")]
-#[aoc::asserts("", "")]
+#[aoc::asserts("0", "0")]
 fn main(input: String, line_ending: &str) -> (usize, usize) {
-    let line_count = input.lines().count();
-    let mut parsed_data = input.split(line_ending);
+    let _line_count = input.lines().count();
+    let mut _parsed_data = input.split(line_ending);
 
     (0, 0)
 }
